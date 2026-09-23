cc0a04777e190132a1de127bca77672b9555a131
Your Name <you@example.com>
2026-09-23 15:57:45 +0200

    solver ajoute pour trouver t

--- src/solver_eq/solver.c
+++ src/solver_eq/solver.c
@@ -6,25 +6,43 @@
 /*   By: bribot <bribot@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/18 11:56:56 by bribot            #+#    #+#             */
-/*   Updated: 2026/09/18 17:22:02 by bribot           ###   ########.fr       */
+/*   Updated: 2026/09/23 15:52:25 by bribot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "miniRT.h"
-float	make_dot_product(float first[3], float second[3])
+double	make_dot_product(double first[3], double second[3])
 {
-	float	to_return;
+	double	to_return;
 
 	to_return = first[0] * second[0] + first[1] * second[1] + first[2] * second[2];
 	return (to_return);
 }
 
-t_solutions	*solver(float x_dir, float y_dir, t_head_objects *head_of_all)
+double	find_lowest_soluc(double a, double b, double c)
 {
-	float	CO[3];
-	float	D[3];
-	float	rayon;
-	t_trifloat	eq;
+	double rc;
+	double	sol1;
+	double	sol2;
+
+	rc = (b * b) - (4 * a * c);
+	if (rc < 0)
+		return (-1000); // sert a se rendre compte q il n y a pas de solutions
+	sol1 = (-b + rc) / (2 * a);
+	sol2 = (-b - rc) / (2 * a);
+
+	if (sol1 <= sol2)
+		return (sol1);
+	else
+		return (sol2);
+}
+
+double solver(double x_dir, double y_dir, t_head_objects *head_of_all)
+{
+	double	CO[3];
+	double	D[3];
+	double	rayon;
+	t_tridouble	eq;
 
 	CO[0] = head_of_all->C->coordinates[0] - head_of_all->first_object->coordinates[0];
 	CO[1] = head_of_all->C->coordinates[1] - head_of_all->first_object->coordinates[1];
@@ -34,8 +52,7 @@ t_solutions	*solver(float x_dir, float y_dir, t_head_objects *head_of_all)
 	D[2] = 1;
 	rayon = (head_of_all->first_object->diameter / 2) * (head_of_all->first_object->diameter / 2);
 	eq.a = make_dot_product(D, D);
-	eq.b = make_dot_product(CO, D);
-	eq.c = make_dot_product(CO, CO);
-	//reste a faire la fonction qui resout le a^2t + bt + c
-	return (NULL);
+	eq.b = make_dot_product(CO, D) * 2;
+	eq.c = make_dot_product(CO, CO) - rayon;
+	return (find_lowest_soluc(eq.a, eq.b, eq.c));
 }
